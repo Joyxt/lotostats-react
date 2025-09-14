71680efbc93df1e8ede3b35054b1f65692d9d698
JOYxt <JoyXT@protonmail.com>
2025-09-15 00:26:45 +0200

    chnage

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// ⚡ Configuration spéciale GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: '/lotostats-react/', // ⚡ doit correspondre au nom exact de ton repo GitHub
+  base: '/lotostats-react/', // doit être EXACTEMENT le nom de ton repo GitHub
 })
